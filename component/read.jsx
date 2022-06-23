b11942ef2eff1de4d56ad6223b700e8286de1a8e
Ana <72501453+anaduartx@users.noreply.github.com>
2022-06-23 11:17:50 -0300

    Add files via upload

--- component/read.jsx
+++ component/read.jsx
@@ -1,83 +1,94 @@
-import Head from 'next/head'
 import { useState,useEffect} from 'react'
-
-//importar a config do firebase
 import { app, database } from '../services/firebase'
-import { collection,addDoc, getDocs, orderBy, query, doc, deleteDoc, getDoc } from 'firebase/firestore';
-
-//configurar o Firebase do projeto
+import { collection,addDoc, getDocs, orderBy, query, doc, deleteDoc, getDoc, updateDoc } from 'firebase/firestore';
 const contato = collection(database,'contato')
-
 export default function Read() {
-
   const [contatoLista,setContatoLista] = useState([])
   const read = ()=>{
-    getDocs(query(contato, orderBy("nome")))
+  getDocs(query(contato,orderBy("nome")))
     .then((data)=>{
       setContatoLista(data.docs.map((item)=>{
         return{...item.data(), id:item.id}
       }))
     })
   }
-
   useEffect(()=>{
     read()
   },[])
-
-  //função botão excluir
+  // Função do botão excluir
   const deleteBtn = (id)=>{
-    const deleteDocumento = doc(database,"contato",id)
-    deleteDoc(deleteDocumento)
+    const documento = doc(database,"contato",id)
+    deleteDoc(documento)
     .then(()=>{
-      read()
+    read()
     })
   }
-
-  //rotina de update inicio
-
-  //mostrar o contato selecionado
-  const [ID,setID] = useState(null)
-  const [contatoUnico,setContatoUnico]=useState[{}]
-  const [mostrar,setMostrar] = useState(false)
-  const [nome,setNome]=useState("")
-  const [email,setEmail]=useState("")
-  const [telefone,setTelefone]=useState("")
-  const [mensagem,setMensagem]=useState("")
-
-  const show = async(id)=>{
-    setID(id)
-    if(ID!=null){
-      const contatoSimples = doc(database,"contato",ID)
-      const resultado = await getDoc(contatoSimples)
-      setContatoUnico({...resultado.data(),id:resultado.id})
-      setMostrar(true)
-    }
+// Rotina de Update início
+// Mostrar o contato selecionado
+const [ID, setID]=useState(null)
+const [contatoUnico,setContatoUnico]=useState({})
+const [mostrar,setMostrar] = useState(false)
+const [nome,setNome] = useState("")
+const [email, setEmail] = useState("")
+const [telefone, setTelefone] = useState("")
+const [mensagem,setMensagem] = useState("")
+const show = async(id)=>{
+  setID(id)
+  if(ID!=null){
+    const contatoSimples = doc(database,"contato",ID)
+    const resultado = await getDoc(contatoSimples)
+    setContatoUnico({...resultado.data(),id:resultado.id})
+    setNome(contatoUnico.nome)
+    setEmail(contatoUnico.email)
+    setTelefone(contatoUnico.telefone)
+    setMensagem(contatoUnico.mensagem)
+    //setMostrar(true)
   }
-  useEffect(()=>{
-    show(ID)
-  },[ID])
-
-  //rotina de update fim
-
+  if(mensagem!=""){
+    setMostrar(true)
+  }
+}
+useEffect(()=>{
+  show()
+},[ID])
+const bt_cancelar = ()=>{
+  setMostrar(false)
+  setNome("")
+  setEmail("")
+  setTelefone("")
+  setMensagem("")
+  setID(null)
+}
+const bt_alterar = (id)=>{
+  const contatoShow = doc(database,"contato",id)
+  updateDoc(contatoShow,{
+    nome: nome, email: email, telefone: telefone, mensagem: mensagem
+  }).then(()=>{
+    setNome("")
+    setEmail("")
+    setTelefone("")
+    setMensagem("")
+    setID(null)
+    read()
+    setMostrar(false)
+  })
+}
+// Rotina de Update fim
   return (
     <>
     {mostrar ?(
       <div>
         <h3 className="text-center">ALTERAR</h3>
-        <input type="text" name="nome" placeholder='Nome' className='form-control' id="" required onChange={event=>setNome(event.target.value)} value={contatoUnico.nome} />
-
-        <input type="email" name="email" placeholder='Email' className='form-control' id="" required onChange={event=>setEmail(event.target.value)} value={contatoUnico.email} />
-
-        <input type="tel" name="telefone" placeholder='Telefone' className='form-control' id="" required onChange={event=>setTelefone(event.target.value)} value={contatoUnico.telefone} />
-
-        <textarea name="mensagem" className='form-control' placeholder='Mensagem' id="" onChange={event=>setMensagem(event.target.value)} value={contatoUnico.mensagem} ></textarea>
-
-        <input type="submit" value="SALVAR" onClick={cadastrar} className='form-control btn btn-outline-dark' />
+        <input type="text" name="nome" placeholder='Nome' className='form-control' id="" required onChange={event=>setNome(event.target.value)} value={nome} />
+        <input type="email" name="email" placeholder='Email' className='form-control' id="" required onChange={event=>setEmail(event.target.value)} value={email} />
+        <input type="tel" name="telefone" placeholder='Telefone' className='form-control' id="" required onChange={event=>setTelefone(event.target.value)} value={telefone} />
+        <textarea name="mensagem" className='form-control' placeholder='Mensagem' id="" onChange={event=>setMensagem(event.target.value)} value={mensagem} ></textarea>
+        <input type="button" value="CANCELAR" className="form-control btn btn-outline-danger" onClick={bt_cancelar} />
+        <input type="submit" value="SALVAR" className='form-control btn btn-outline-dark' onClick={()=>bt_alterar(contatoUnico.id)} />
       </div>
     ):(
       <></>
     )}
-
         <h3 className='text-center'>GRAVADOS</h3>
         {contatoLista.map((lista)=>{
           return(
@@ -90,12 +101,11 @@ export default function Read() {
               </div>
               <div className='card-footer text-center'>
               <div className="input-group">
-                <input type="button" className='btn-outline-warning form-control' value="Alterar" onclick={()=>show(lista.id)}/>
-              <input type="button" onClick={()=>deleteBtn(lista.id)} className='btn-outline-danger form-control' value="Excluir" />  
+              <input type="button" className='btn-outline-warning form-control' value="Alterar" onClick={()=>show(lista.id)} />
+              <input type="button" className='btn-outline-danger form-control' value="Excluir" onClick={()=>deleteBtn(lista.id)} />
               </div>
               </div>
-
-              </div>
+            </div>
           )
         })}
     </>
